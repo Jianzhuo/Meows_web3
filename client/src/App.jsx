f1018b380b57cfa3b24291f4c56b0bfe06c8a8c0
Jianzhuo.Shen <shenjianzhuo@gmail.com>
2021-11-07 11:05:31 +1300

    Start up the WEB 3.0 Project. Init react structure, create components used in front page. Add a folder for smart contract development.

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Navbar, Welcome, Services, Loader, Transactions, Footer } from "./components"
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className='min-h-screen'>
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
