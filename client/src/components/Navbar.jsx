516ed298c7a540ff9e7d08ba1848241d159edabf
Jianzhuo.Shen <shenjianzhuo@gmail.com>
2021-11-09 09:04:22 +1300

    Add Navbar component to client side of the project.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,58 @@
-const Navbar = () => {
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
+
+import logo from '../../images/logo.png';
+
+{/* 
+  Function component to handle the menu item used in Navbar menu.
+*/}  
+const NavBarItem = ({ title, classprops }) => {
     return (
-        <h1>Navbar</h1>
+        <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+    )
+};
+  
+{/* 
+  Navbar component deal with logo and menu, have 2 layout suit for mobile and desktop devices.
+*/}
+const Navbar = () => {
+    {/* Moblie version Menu State (open or close). */}
+    const [toggleMenu, setToggleMenu] = useState(false);
+    {/* State is Navbar component first time loaded. */}
+    const [init, setInit] = useState(false);
+
+    return (
+        <nav>
+            <div className="w-full flex md:justify-center justify-between items-center p-4">
+                <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                    <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+                </div>
+                <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                    {["Ecosystem", "Buy", "NFT/Art", "Community"].map((item, index) => (
+                        <NavBarItem key={item + index} title={item} />
+                    ))}
+                    <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                        Connect wallet
+                    </li>
+                </ul>
+                <div className="flex relative">
+                    {toggleMenu
+                        ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => {setToggleMenu(false);}} />
+                        : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => {setToggleMenu(true); !init && setInit(true);}} />}
+                </div>
+            </div>
+            <div>
+                {init && (
+                    <ul id="menuMobile" className={`z-10 p-3 fixed w-full md:hidden h-screen list-none
+                        flex flex-col justify-start items-end text-white white-glassmorphism ${toggleMenu ? "animate-slide-in" : "animate-slide-out"}`}>
+                        {["Ecosystem", "Buy", "NFT/Art", "Community"].map((item, index) => (
+                            <NavBarItem key={item + index} title={item} classprops="my-2 text-2xl font-bold" />
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </nav>
     );
 }
 
